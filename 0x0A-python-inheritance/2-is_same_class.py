bbca0c7370ab119fc385991389e3950dfb877b49
sylverlyn <msylverlyn@gmail.com>
2023-01-10 14:14:13 -0800

    fix codes

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,4 +7,6 @@ def is_same_class(obj, a_class):
     """This  function that returns True if the object is exactly
     an instance of the specified class ; otherwise False."""
 
-    return callable(is_same_class)
+    if isinstance(obj, a_class):
+        return True
+    return False
