9255fe377563fb27f2e98e52208bdbc8eaf3dda3
sylverlyn <msylverlyn@gmail.com>
2023-01-10 14:02:51 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""returns the list of available attributes and methods of an object"""
+
+
+def lookup(obj):
+    """This function returns list of available attributes and methods."""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""This method checks if an obj is an instance of a class."""
+
+
+def is_same_class(obj, a_class):
+    """This  function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False."""
+
+    return callable(is_same_class)
