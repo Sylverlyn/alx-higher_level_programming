f467539c907cfb93d9d1bd0d9530a73eb3ce1da9
SwiftCoder <111044756+Sylverlyn@users.noreply.github.com>
2023-01-13 16:38:25 +0100

    Delete 0x08-python-more_classes directory

--- 0x08-python-more_classes/0-rectangle.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-
-"""Defines a rectangle class."""
-
-
-class Rectangle:
-    """parse nothing"""
-    pass
--- 0x08-python-more_classes/README.md
+++
@@ -1,1 +0,0 @@
-This project is on classes and objects in python language.
