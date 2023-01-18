391d1f68a88b6f119ba3222c21b7c0777674aca4
Sylverlyn Mbaogu <msylverlyn@gmail.com>
2023-01-18 09:57:00 +0100

    add new file

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("{}".format(chr(i)), end='')
+for i in range (97, 123):
+    if char == ord('q') or char == ord('e'):
+        continue
+    print("{:c}".format(char), end='')
