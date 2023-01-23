0b0b55a271a7f2afba128a96543bb1e5d6a2e8b8
sylverlyn <msylverlyn@gmail.com>
2023-01-23 11:41:50 -0800

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,24 +2,13 @@
 def safe_print_list(my_list=[], x=0):
 
     list = 0
-<<<<<<< HEAD
 
-=======
->>>>>>> 96b3d4afc8e48ad43b0e25245c149f34d5e26cef
     for i in range(x):
         try:
             print(f'{my_list[i]}', end='')
             list += 1
-<<<<<<< HEAD
-=======
-
->>>>>>> 96b3d4afc8e48ad43b0e25245c149f34d5e26cef
         except IndexError:
             pass
 
     print()
-<<<<<<< HEAD
     return list
-=======
-    return list
->>>>>>> 96b3d4afc8e48ad43b0e25245c149f34d5e26cef
