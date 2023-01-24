f3b699bf1c10569052ae46675e9b315bc3dd28e9
sylverlyn <msylverlyn@gmail.com>
2023-01-24 02:13:23 -0800

    private instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a square."""
+
+
+class Square:
+    """creates a class."""
+    def __init__(self, size):
+        """initializes an instance."""
+        self.__size = size
