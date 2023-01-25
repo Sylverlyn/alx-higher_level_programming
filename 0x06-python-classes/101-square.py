6e07d65ab2302c350c92f435d9605243ec5122f4
sylverlyn <msylverlyn@gmail.com>
2023-01-24 23:48:03 -0800

    fix line 44

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -41,7 +41,7 @@ class Square:
     def size(self, value):
         """sets/assigns a new value to the attribute."""
         if type(value) is not int:
-            raise TypeError("size must be an integer")
+            raise ValueError("size must be an integer")
         elif value < 0:
             raise ValueError('size must be >= 0')
         self.__size = value
