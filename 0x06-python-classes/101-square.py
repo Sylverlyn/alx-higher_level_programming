cac00a5e7193457e9fa5357caa893f963d0dea9c
sylverlyn <msylverlyn@gmail.com>
2023-01-25 14:17:17 -0800

    fix extra newline

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -56,6 +56,5 @@ class Square:
             square += "\n"
         for j in range(self.__size):
             square += " "*self.__position[0]
-            square += "#"*self.__size
-            square += "\n"
-        return square
+            square += "#"*self.__size + "\n"	    
+        return square[:-1]
