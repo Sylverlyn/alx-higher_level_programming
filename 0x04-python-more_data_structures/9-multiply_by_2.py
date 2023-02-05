5ab6ffa1fd6aec040f7c1b414f6f57be8a415226
sylverlyn <msylverlyn@gmail.com>
2023-02-05 15:13:48 -0800

    fix line 6

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     new_dict = a_dictionary.copy()
     for key, value in new_dict.items():
         new_dict[key] = value * 2
-    return new_dict	
+    return new_dict
