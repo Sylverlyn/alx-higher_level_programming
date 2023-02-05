0e7f59bcb9c791fd11706f4845f30bc22653fea7
sylverlyn <msylverlyn@gmail.com>
2023-02-05 02:18:52 -0800

    retry codes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [[num ** 2 for num in row] for row in matrix]
-    return new
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+        for num in row:
+            new_row.append(num ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
