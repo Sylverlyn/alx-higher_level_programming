33b600de7525d8a965a237e7ce83b885589f785f
sylverlyn <msylverlyn@gmail.com>
2023-02-06 14:09:47 -0800

    raise exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module creates an empty class."""
+
+
+class BaseGeometry():
+    """Nothing is passed."""
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
