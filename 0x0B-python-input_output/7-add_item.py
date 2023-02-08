722aace82876d50282a84953dad07f34f9c2c42e
sylverlyn <msylverlyn@gmail.com>
2023-02-08 14:13:00 -0800

    hot fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,12 +9,12 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = "add_item.json"
-json_list = []
 
-if os.path.exists(file):
-    json_list = load_from_json_file(file)
+if os.path.isfile(file):
+    my_list = load_from_json_file(file)
 
+my_list = []
 for i in range(1, len(sys.argv)):
-    json_list.append(sys.argv[i])
+    my_list.append(sys.argv[i])
 
-save_to_json_file(json_list, file)
+save_to_json_file(my_list, file)
