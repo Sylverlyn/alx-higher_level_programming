d89ac7b046dfaf374dfe7e9d4bd773101cef6daa
sylverlyn <msylverlyn@gmail.com>
2023-02-09 07:18:16 -0800

    import json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,9 @@
 #!usr/bin/python3
 """class to object."""
+import json
 
 
 def class_to_json(obj):
     """returns data structure for JSON
     serialization of object."""
-    return JSON.dumps(obj.__dict__.copy())
+    return json.dumps(obj.__dict__.copy())
