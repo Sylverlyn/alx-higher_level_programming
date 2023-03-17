d2ed4c417a24f92507306e6908885ac95b59a1d0
sylverlyn <msylverlyn@gmail.com>
2023-03-17 15:10:33 -0700

    assign unique

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,8 @@
 -- creates the table unique_id
-CREATE TABLE IF NOT EXITS unique_id(
-id INT DEFAULT 1 UNIQUE,
-name VARCHAR(256) NOT NULL
-);
+-- asigns id a default number
+
+CREATE TABLE
+IF NOT EXITS unique_id(
+  id INT DEFAULT 1 UNIQUE,
+  name VARCHAR(256) NOT NULL
+  );
