c7057a28ace9d0abec5d7ccd3b90bed107f045f8
sylverlyn <msylverlyn@gmail.com>
2023-03-17 14:47:57 -0700

    grant privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of the MySQL users 
+-- lists all privileges of the MySQL users
+-- use of grant statements
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
