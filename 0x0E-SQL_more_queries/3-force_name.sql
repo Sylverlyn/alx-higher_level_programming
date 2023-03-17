1eb387c07af5cdebafc67f62745ab973d17d021f
sylverlyn <msylverlyn@gmail.com>
2023-03-17 15:02:33 -0700

    create tables

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,9 +1,9 @@
---  creates the table force_name
+i--  creates the table force_name
 -- force_name description:
 -- id INT
 -- name VARCHAR(256) can’t be null
 
 CREATE TABLE IF NOT EXISTS force_name(
-id INT
+id INT,
 name VARCHAR(256) NOT NULL
 );
