acb1cb7affdc82f3a3a9f5d4fec04b6fc0fb27bc
Sylverlyn <msylverlyn@gmail.com>
2023-04-14 03:59:51 +0100

    javascript scripts

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const x = parseInt(process.argv.slice(2));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  let i = 0;
+  let i = 1;
   while (i <= x) {
     console.log('C is fun');
     i++;
